5ce250e2a0f2473ce93881b609a42284e5ddcc6e
Skyler Giannetti <sjg5901@psu.edu>
2023-06-28 22:11:47 +0000

    Return Array Contents challenge complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String temp = "";
+
+        for (int i = 0; i < nums.length; i++) {
+            temp += nums[i] + " ";
+        }
+
+        return temp.trim();
     }
 }
